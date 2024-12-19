239b8fd4319f52421f3be8c510d2dd1705c51e66
Max Shundrik <m.shundrick@yandex.ru>
2024-12-20 01:30:02 +0300

    Fixed some methods according Redis Cache. Deleted all @Cacheable annotations

--- src/main/java/com/xrc/mcs/services/RoomCategoryService.java
+++ src/main/java/com/xrc/mcs/services/RoomCategoryService.java
@@ -1,10 +1,10 @@
 package com.xrc.mcs.services;
 
 import com.xrc.mcs.entity.RoomCategory;
+import com.xrc.mcs.repository.ProtectionCacheRepository;
 import com.xrc.mcs.repository.RoomCategoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,15 +15,29 @@ import java.util.List;
 public class RoomCategoryService {
     private final RoomCategoryRepository roomCategoryRepository;
     private final DmdService dmdService;
+    private final ProtectionCacheRepository pcRepository;
 
-    @Cacheable(value = "roomCategories")
     public List<String> findAll() {
+        List<String> list = pcRepository.getFromCache("roomCategories", List.class);
+        if (list == null) {
+
+            list = roomCategoryRepository.findAll().stream().map(RoomCategory::getName).toList();
+            log.info("findAll roomCategories in DB and put into cache");
+            pcRepository.saveToCache("roomCategories", list);
+        }
         log.info("find all room categories if DB");
-        return roomCategoryRepository.findAll().stream().map(RoomCategory::getName).toList();
+        return list;
     }
-    @Cacheable(value = "dmdByRoomCategory", key = "#roomCategoryName")
+
     public double findDmdByRoomCategoryName(String roomCategoryName) {
-        log.info("looking for a dmd value for: {} in DB", roomCategoryName);
-        return dmdService.getDmdByRoomCategory(roomCategoryName);
+        Double result = pcRepository.getFromCache("dmdFor:" + roomCategoryName, Double.class);
+        if (result == null) {
+            log.info("looking for a dmd value for: {} in DB", roomCategoryName);
+            result = dmdService.getDmdByRoomCategory(roomCategoryName);
+            log.info("dmd for category \"{}\" was saved to cache", roomCategoryName);
+            pcRepository.saveToCache("dmdFor:" + roomCategoryName, result);
+        }
+
+        return result;
     }
 }
